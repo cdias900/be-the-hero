ad38d256b4e8b63ba996e5b6c32f605e14b6ccde
cdias900 <pedro@cdias.dev>
2020-03-26 15:56:37 -0300

    Altera editorconfig do backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -4,15 +4,15 @@ const connection = require('../database/connection');
 module.exports = {
   async index(req, res) {
     const ongs = await connection('ongs').select('*');
-  
+
     return res.json(ongs);
   },
-  
+
   async create(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-  
+
     const id = crypto.randomBytes(4).toString('HEX');
-  
+
     await connection('ongs').insert({
       id,
       name,
@@ -21,7 +21,7 @@ module.exports = {
       city,
       uf,
     })
-  
+
     return res.json({ id });
   },
 };
